9d405e39e68b6b30add2051dcd42fdac4c6c68d5
ramazan yaraliev <yaru@bk.ru>
2019-02-09 18:06:14 +0300

    edit

--- task2.rb
+++ task2.rb
@@ -1,25 +1,22 @@
-class Array
-  def middle
-    middle_index = (self.length / 2).round
-    self[middle_index]
-  end
-end
-
-class String
-  def plural(symbol)
-    self.end_with?('s') ? self : "#{self}#{symbol}"
+module ExtArray
+  def middle(array)
+    middle_index = (array.length / 2).round
+    array[middle_index]
   end
 end
 
 class Main
+  include ExtArray
+
+  def plural(str, symbol)
+    str.end_with?('s') ? str : "#{str}#{symbol}"
+  end
+
   def main
     files = Dir.entries('.')
-    puts files
-           .select { |file| file.start_with?('.') }
-           .sort
-           .middle
-           .plural('s')
-           .upcase
+    dot_sorted_files = files.select { |file| file.start_with?('.') }.sort
+    middle_file = middle(dot_sorted_files)
+    puts plural(middle_file, 's').upcase
   end
 end
 
