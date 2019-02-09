35eb8d18294964540925d35e057d6f3967d3980a
ramazan yaraliev <yaru@bk.ru>
2019-02-09 18:19:32 +0300

    remove module

--- task2.rb
+++ task2.rb
@@ -1,23 +1,21 @@
-module ExtArray
+class Main
+  def main
+    files = Dir.entries('.')
+    dot_sorted_files = files.select { |file| file.start_with?('.') }.sort
+    middle_file = middle(dot_sorted_files)
+    plural(middle_file, 's').upcase
+  end
+
+  private
+
+  def plural(str, symbol)
+    str.end_with?('s') ? str : "#{str}#{symbol}"
+  end
+
   def middle(array)
     middle_index = (array.length / 2).round
     array[middle_index]
   end
 end
 
-class Main
-  include ExtArray
-
-  def plural(str, symbol)
-    str.end_with?('s') ? str : "#{str}#{symbol}"
-  end
-
-  def main
-    files = Dir.entries('.')
-    dot_sorted_files = files.select { |file| file.start_with?('.') }.sort
-    middle_file = middle(dot_sorted_files)
-    puts plural(middle_file, 's').upcase
-  end
-end
-
-Main.new.main
+puts Main.new.main
